3309fd10c84cac32edb702ea0f98896c0a983047
Vangari Karthik <132283225+Vangarikarthik@users.noreply.github.com>
2024-04-25 19:11:23 +0530

    Add files via upload

---
+++ services/laptop_microservice/laptopData.js
@@ -0,0 +1,33 @@
+const laptopProducts = [
+    {
+      id: 1,
+      name: "MSI Bravo 15 ",
+      imageUrl: "https://rukminim2.flixcart.com/image/832/832/xif0q/computer/b/j/7/bravo-15-b5ed-notebook-msi-original-imagz3rya2qqbhjt.jpeg?q=70&crop=false",
+      description: "Ryzen 5 Hexa Core 7535HS - (8 GB/512 GB SSD/Windows 11 Home/4 GB Graphics/AMD Radeon RX6550M/144 Hz) Bravo 15 B7ED-012IN Gaming Laptop  (15.6 inch, Black, 2.35 Kg)",
+      price: "₹49,990",
+      link: "https://www.flipkart.com/msi-bravo-15-ryzen-5-hexa-core-7535hs-8-gb-512-gb-ssd-windows-11-home-4-graphics-amd-radeon-rx6550m-144-hz-b7ed-012in-gaming-laptop/p/itmc0df4058dba89?pid=COMGSZ65H4YCYY7M&lid=LSTCOMGSZ65H4YCYY7MGILCHL&marketplace=FLIPKART",
+      category: "gaming",
+    },
+    {
+      id: 2,
+      name: "ZEBRONICS Pro Series Y",
+      imageUrl: "https://rukminim2.flixcart.com/image/832/832/xif0q/computer/s/p/n/-original-imagxqetzpemafuw.jpeg?q=70&crop=false",
+      description: "Intel Core i5 11th Gen 1155G7 - (16 GB/512 GB SSD/Windows 11 Home) ZEB-NBC 2S Thin and Light Laptop  (15.6 inch, Silver, 1.65 kg)",
+      price: "₹27,990",
+      link: "https://www.flipkart.com/zebronics-pro-series-y-intel-core-i5-11th-gen-1155g7-16-gb-512-gb-ssd-windows-11-home-zeb-nbc-2s-thin-light-laptop/p/itmf6dbf61fa8378?pid=COMGTABCGCGD8ZDZ&lid=LSTCOMGTABCGCGD8ZDZ8X3AN6&marketplace=FLIPKART&q=zebronics+laptops&store=6bo%2Fb5g&srno=s_1_3&otracker=AS_QueryStore_OrganicAutoSuggest_1_9_na_na_ps&otracker1=AS_QueryStore_OrganicAutoSuggest_1_9_na_na_ps&fm=search-autosuggest&iid=68c92574-f9ff-425a-9e12-9c1895c5a631.COMGTABCGCGD8ZDZ.SEARCH&ppt=sp&ppn=sp&ssid=p1spm86ngw0000001711773309518&qH=a271e8b42ae1eee2",
+      category: "study",
+    },
+    {
+      id: 3,
+      name: "mac",
+      imageUrl: "https://rukminim2.flixcart.com/image/832/832/xif0q/computer/s/p/n/-original-imagxqetzpemafuw.jpeg?q=70&crop=false",
+      description: "Intel Core i5 11th Gen 1155G7 - (16 GB/512 GB SSD/Windows 11 Home) ZEB-NBC 2S Thin and Light Laptop  (15.6 inch, Silver, 1.65 kg)",
+      price: "₹97,990",
+      link: "https://www.flipkart.com/zebronics-pro-series-y-intel-core-i5-11th-gen-1155g7-16-gb-512-gb-ssd-windows-11-home-zeb-nbc-2s-thin-light-laptop/p/itmf6dbf61fa8378?pid=COMGTABCGCGD8ZDZ&lid=LSTCOMGTABCGCGD8ZDZ8X3AN6&marketplace=FLIPKART&q=zebronics+laptops&store=6bo%2Fb5g&srno=s_1_3&otracker=AS_QueryStore_OrganicAutoSuggest_1_9_na_na_ps&otracker1=AS_QueryStore_OrganicAutoSuggest_1_9_na_na_ps&fm=search-autosuggest&iid=68c92574-f9ff-425a-9e12-9c1895c5a631.COMGTABCGCGD8ZDZ.SEARCH&ppt=sp&ppn=sp&ssid=p1spm86ngw0000001711773309518&qH=a271e8b42ae1eee2",
+      category: "show off",
+    },
+    
+    
+  ];
+  
+  module.exports = laptopProducts;
