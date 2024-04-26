3c498ee4bab5e54f3f8029e3794366f14763923e
Vangari Karthik <132283225+Vangarikarthik@users.noreply.github.com>
2024-04-26 18:37:29 +0530

    Add files via upload

---
+++ services/mobile_microservice/mobileData.js
@@ -0,0 +1,43 @@
+const mobileProducts = [
+    {
+      id: 1,
+      name: "poco X6 pro 5g",
+      imageUrl: "https://rukminim2.flixcart.com/image/832/832/xif0q/mobile/j/i/m/-original-imagwzrjtrcdbebe.jpeg?q=70&crop=false",
+      description: "8 GB RAM 256 GB ROM 16.94 cm(6.67 inch) Display 64MP + 8MP + 2MP 16MP Front Camera 5000 mAh Battery Dimensity D8300 Ultra Processor",
+      price: "₹25,999",
+      link: "https://www.flipkart.com/poco-x6-pro-5g-yellow-256-gb/p/itm46a0b51f57a68?pid=MOBGWMGBEVDXXEZC&affid=adminsmart1&affExtParam1=U8P7OsR001X7Spv",
+      category: "gaming",
+    },
+    {
+      id: 2,
+      name: "vivo X100 Pro",
+      imageUrl: "https://rukminim2.flixcart.com/image/832/832/xif0q/mobile/f/m/s/-original-imagwyhjdvhyujhg.jpeg?q=70&crop=false",
+      description: "16 GB RAM | 512 GB ROM 17.22 cm (6.78 inch) Full HD+ Display 50MP + 50MP + 50MP | 32MP Front Camera 5400 mAh Battery Dimensity 9300 Processor",
+      price: "₹89,999",
+      link: "https://www.flipkart.com/vivo-x100-pro-asteroid-black-512-gb/p/itm798a26f6edb96?pid=MOBGWH8SQGGQM2CA&lid=LSTMOBGWH8SQGGQM2CAMU5P1P&marketplace=FLIPKART&q=vivo+x100+pro&store=tyy%2F4io&srno=s_1_1&otracker=AS_QueryStore_OrganicAutoSuggest_1_9_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_9_na_na_na&fm=organic&iid=1e67fbe0-a8be-4cec-9b8b-22ddbaf13043.MOBGWH8SQGGQM2CA.SEARCH&ppt=pp&ppn=pp&ssid=loq4v14uzk0000001711771674774&qH=c667bc655105bdcc",
+      category: "camera",
+    },
+    {
+      id: 3,
+      name: "lg g8x",
+      imageUrl: "https://rukminim2.flixcart.com/image/832/832/xif0q/mobile/f/m/s/-original-imagwyhjdvhyujhg.jpeg?q=70&crop=false",
+      description: "snap dragon 855 4000 mah battery",
+      price: "₹35,999",
+      link: "https://www.flipkart.com/vivo-x100-pro-asteroid-black-512-gb/p/itm798a26f6edb96?pid=MOBGWH8SQGGQM2CA&lid=LSTMOBGWH8SQGGQM2CAMU5P1P&marketplace=FLIPKART&q=vivo+x100+pro&store=tyy%2F4io&srno=s_1_1&otracker=AS_QueryStore_OrganicAutoSuggest_1_9_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_9_na_na_na&fm=organic&iid=1e67fbe0-a8be-4cec-9b8b-22ddbaf13043.MOBGWH8SQGGQM2CA.SEARCH&ppt=pp&ppn=pp&ssid=loq4v14uzk0000001711771674774&qH=c667bc655105bdcc",
+      category: "mine",
+    },
+
+    {
+      id: 4,
+      name: "iphone 6",
+      imageUrl: "https://rukminim2.flixcart.com/image/832/832/xif0q/mobile/f/m/s/-original-imagwyhjdvhyujhg.jpeg?q=70&crop=false",
+      description: "snap dragon 855 4000 mah battery",
+      price: "₹36,999",
+      link: "https://www.flipkart.com/vivo-x100-pro-asteroid-black-512-gb/p/itm798a26f6edb96?pid=MOBGWH8SQGGQM2CA&lid=LSTMOBGWH8SQGGQM2CAMU5P1P&marketplace=FLIPKART&q=vivo+x100+pro&store=tyy%2F4io&srno=s_1_1&otracker=AS_QueryStore_OrganicAutoSuggest_1_9_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_9_na_na_na&fm=organic&iid=1e67fbe0-a8be-4cec-9b8b-22ddbaf13043.MOBGWH8SQGGQM2CA.SEARCH&ppt=pp&ppn=pp&ssid=loq4v14uzk0000001711771674774&qH=c667bc655105bdcc",
+      category: "teja",
+    },
+    
+  ];
+  
+  module.exports = mobileProducts;
+  
